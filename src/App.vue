ccd4f0d7f615775b3d7f59a4b022d9605e06f2be
Manuel <manuel.buenviajeii@gmail.com>
2026-02-14 01:04:32 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="app">
+    <h1>{{ message }}</h1>
+    <button @click="count++">Count is: {{ count }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello Vue with Vanilla JS!',
+      count: 0
+    }
+  }
+}
+</script>
+
+<style>
+.app {
+  font-family: Arial, sans-serif;
+  text-align: center;
+  margin-top: 50px;
+}
+button {
+  padding: 10px 20px;
+  font-size: 16px;
+}
+</style>
