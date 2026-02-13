800f0a60d83697e177a44bf1bc45e8f601473322
Manuel <manuel.buenviajeii@gmail.com>
2026-02-14 02:00:39 +0800

    added solution

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { resolve } from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/DreamNote29/', // <--- IMPORTANTE: Ito ang repository name mo
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src')
